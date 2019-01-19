bb74698a751ace3df44ab6693c6a6a67ec2e822d
reneeb <info@perl-services.de>
2019-01-19 07:54:20 +0100

    add support for standardish <OTRS_...> placeholders

--- Custom/Kernel/Output/HTML/FilterElementPost/StdSubject.pm
+++ Custom/Kernel/Output/HTML/FilterElementPost/StdSubject.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2016 Perl-Services.de, http://www.perl-services.de/
+# Copyright (C) 2016 - 2019 Perl-Services.de, http://www.perl-services.de/
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -65,17 +65,24 @@ sub Run {
         >
     }xms;
 
-    if ( $HasTags ) {
-        my %Ticket = $TicketObject->TicketGet(
-            TicketID => $TicketID,
-        );
+    my %Ticket = $TicketObject->TicketGet(
+        TicketID => $TicketID,
+    );
 
+    if ( $HasTags ) {
         $Translated =~ s{
             <
                 (TicketNumber|Title)
             >
         }{$Ticket{$1}}xmsg;
     } 
+
+    $Translated = $Self->_ReplaceMacros(
+        Text           => $Translated,
+        UserID         => $Self->{UserID} || $LayoutObject->{UserID},
+        Ticket         => \%Ticket,
+	LanguageObject => $LanguageObject,
+    );
 
     my $SubjectContent = $LayoutObject->Output(
         Template => '[% Data.Subject | html %]',
@@ -90,5 +97,106 @@ sub Run {
 
     return 1;
 }
+
+sub _ReplaceMacros {
+    my ( $Self, %Param ) = @_;
+
+    my $LogObject       = $Kernel::OM->Get('Kernel::System::Log');
+    my $ConfigObject    = $Kernel::OM->Get('Kernel::Config');
+    my $UserObject      = $Kernel::OM->Get('Kernel::System::User');
+    my $HTMLUtilsObject = $Kernel::OM->Get('Kernel::System::HTMLUtils');
+
+    # check needed stuff
+    for my $Needed (qw(Text UserID Ticket LanguageObject)) {
+        if ( !defined $Param{$Needed} ) {
+            $LogObject->Log(
+                Priority => 'error',
+                Message  => "Need $Needed!",
+            );
+            return;
+        }
+    }
+
+    my $Text = $Param{Text};
+
+    # determine what "macro" delimiters are used
+    my $Start = '<';
+    my $End   = '>';
+    my $NL    = "\n";
+
+    my $LanguageObject = $Param{LanguageObject};
+
+    my %TicketData = %{ $Param{Ticket} };
+    for my $Field (qw(State Priority)) {
+        $TicketData{$Field} = $LanguageObject->Translate( $TicketData{$Field} );
+    }
+
+    # replace config options
+    my $Tag = $Start . 'OTRS_CONFIG_';
+    $Text =~ s{ $Tag (.+?) $End }{$ConfigObject->Get($1)}egx;
+
+    # cleanup
+    $Text =~ s{ $Tag .+? $End }{-}gi;
+
+    $Tag = $Start . 'OTRS_Agent_';
+    my $Tag2 = $Start . 'OTRS_CURRENT_';
+    my %CurrentUser = $UserObject->GetUserData( UserID => $Param{UserID} );
+
+    # html quoting of content
+    if ( $Param{RichText} ) {
+        KEY:
+        for my $Key ( sort keys %CurrentUser ) {
+            next KEY if !$CurrentUser{$Key};
+            $CurrentUser{$Key} = $HTMLUtilsObject->ToHTML(
+                String => $CurrentUser{$Key},
+            );
+        }
+    }
+
+    # replace it
+    KEY:
+    for my $Key ( sort keys %CurrentUser ) {
+        next KEY if !defined $CurrentUser{$Key};
+        $Text =~ s{ $Tag $Key $End }{$CurrentUser{$Key}}gxmsi;
+        $Text =~ s{ $Tag2 $Key $End }{$CurrentUser{$Key}}gxmsi;
+    }
+
+    # replace other needed stuff
+    $Text =~ s{ $Start OTRS_FIRST_NAME $End }{$CurrentUser{UserFirstname}}gxms;
+    $Text =~ s{ $Start OTRS_LAST_NAME $End }{$CurrentUser{UserLastname}}gxms;
+
+    # cleanup
+    $Text =~ s{ $Tag .+? $End}{-}xmsgi;
+    $Text =~ s{ $Tag2 .+? $End}{-}xmsgi;
+
+    # replace <OTRS_TICKET_... tags
+    {
+        my $Tag = $Start . 'OTRS_TICKET_';
+
+        # html quoting of content
+        if ( $Param{RichText} ) {
+            KEY:
+            for my $Key ( sort keys %TicketData ) {
+                next KEY if !$TicketData{$Key};
+                $TicketData{$Key} = $HTMLUtilsObject->ToHTML(
+                    String => $TicketData{$Key},
+                );
+            }
+        }
+
+        # replace it
+        KEY:
+        for my $Key ( sort keys %TicketData ) {
+            next KEY if !defined $TicketData{$Key};
+            $Text =~ s{ $Tag $Key $End }{$TicketData{$Key}}gxmsi;
+        }
+
+        # cleanup
+        $Text =~ s{ $Tag .+? $End}{-}gxmsi;
+    }
+
+    return $Text;
+}
+
 
 1;
